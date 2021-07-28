af973f843fccdb5db69204f6574d52ddf2485221
Muhire-Josue <rutayisire.josue09@gmail.com>
2021-07-28 18:20:12 +0200

    add create post, posts service

--- posts/index.js
+++ posts/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const {randomBytes} = require('crypto')
+const bodyParser = require('body-parser')
 
 const app = express();
+app.use(bodyParser.json());
+const posts = {};
 
-app.get('posts', (req, res) => {});
-app.post('posts', (req, res) => {});
+app.get('posts', (req, res) => {
+  posts.send(posts);
+});
+app.post('posts', (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const {title} = req.body;
+
+  posts[id] = {id, title}
+});
 
 app.listen(4000, ()=>{console.log('Listening on 4000')});
