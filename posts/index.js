3f6fd8b65fad51f20dca22aece4bc5caee893762
Muhire-Josue <rutayisire.josue09@gmail.com>
2021-08-02 17:19:37 +0200

    add cors, all services

--- posts/index.js
+++ posts/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const {randomBytes} = require('crypto')
-const bodyParser = require('body-parser')
+const {randomBytes} = require('crypto');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 const posts = {};
 
 app.get('/posts', (req, res) => {
@@ -13,7 +15,7 @@ app.post('/posts', (req, res) => {
   const id = randomBytes(4).toString('hex');
   const {title} = req.body;
 
-  posts[id] = {id, title}
+  posts[id] = {id, title};
   res.send(posts);
 });
 
