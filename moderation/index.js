22ba65d98dd4e2f1bd32c80e985bd81041847b5e
Muhire-Josue <rutayisire.josue09@gmail.com>
2021-08-06 16:10:20 +0200

    update comments, comments service

--- moderation/index.js
+++ moderation/index.js
@@ -7,8 +7,20 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
+const {type, data} = req.body;
 
+if (type === 'CommentCreated') {
+  const status = data.content.includes('organce') ? 'rejected' : 'approved';
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentModerated',
+    id: data.id,
+    postId: data.postId,
+    content: data.content,
+    status,
+  });
+}
+return res.send({});
 });
 
 app.listen(4003, () => {console.log('Listening on port 4003');})
