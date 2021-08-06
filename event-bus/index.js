e97c69ffdd1ed044f81b4d470727fe25f2c65a4d
Muhire-Josue <rutayisire.josue09@gmail.com>
2021-08-06 12:03:59 +0200

    add initialize query service

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,24 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
-const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
 
-app.post('/events', async (req, res) => {
-  try {
-    const events = req.body;
-    await axios.post('http://localhost:4000/events', events);
-    await axios.post('http://localhost:4001/events', events);
-    await axios.post('http://localhost:4002/events', events);
-  
-    res.send({status: 200});
-    
-  } catch (error) {
-    console.error(error);
-  }
+app.post('/events', (req, res) => {
+  const event = req.body;
+
+  axios.post('http://localhost:4000/events', event);
+  axios.post('http://localhost:4001/events', event);
+  axios.post('http://localhost:4002/events', event);
+
+  res.send({ status: 'OK' });
 });
 
-app.listen(4005, () => {console.log('Listening on port 4005')});
+app.listen(4005, () => {
+  console.log('Listening on 4005');
+});
